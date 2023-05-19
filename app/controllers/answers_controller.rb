8f824b90fed3126b0f610bc3790abe3d88a9aaba
KEYproducts <keiko.yamamoto51@gmail.com>
2023-05-19 14:43:11 +0900

    回答の一覧表示機能作成

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,16 +1,18 @@
 class AnswersController < ApplicationController
   def create
     @question = Question.find(params[:question_id])
-    @answer = @question.answers.new(answer_params)
+    @answer = Answer.new(answer_params)
+
     if @answer.save
       redirect_to question_path(@question)
     else
+      @answers = @question.answers
       render 'questions/show'
     end
   end
 
   private
   def answer_params
-    params.require(:answer).permit(:content, :user_name)
+    params.require(:answer).permit(:content, :user_name).merge(question_id: params[:question_id])
   end
 end
